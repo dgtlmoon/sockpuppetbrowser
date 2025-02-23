cb4c57b709e76265b40dd35d298d05f3f8eda5e8
dgtlmoon <dgtlmoon@gmail.com>
2025-02-23 19:32:43 +0100

    Adding `child_count` to /stats

--- backend/http_server.py
+++ backend/http_server.py
@@ -1,16 +1,21 @@
 import psutil
 from aiohttp import web
 from loguru import logger
+import os
 
 async def handle_http_request(request, stats):
     svmem = psutil.virtual_memory()
+    parent = psutil.Process(os.getpid())
+    child_count = len(parent.children(recursive=False))  # Only direct children
+
     data = {
         'active_connections': stats['connection_count'],
+        'child_count': child_count,
         'connection_count_total': stats['connection_count_total'],
         'dropped_threshold_reached': stats['dropped_threshold_reached'],
         'dropped_waited_too_long': stats['dropped_waited_too_long'],
         'mem_use_percent': svmem.percent,
-        'special_counter_len': len(stats['special_counter'])
+        'special_counter_len': len(stats['special_counter']),
     }
 
     return web.json_response(data, content_type='application/json')
